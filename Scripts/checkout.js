913892f2a8cd36ba79724dde9b805f4ad0557186
Anshid <anshidpmofficial@gmail.com>
2025-11-28 09:08:56 +0530

    Lesson 17 exercises

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 // import '../data/cart-calss.js';
+import '../data/car.js';
 
 
 renderCheckoutHeader();
