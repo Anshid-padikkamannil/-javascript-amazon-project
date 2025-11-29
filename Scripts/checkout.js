2d0b33d8b9e4f5212a1cdbe95ae17e9bfe380189
Anshid <anshidpmofficial@gmail.com>
2025-11-29 08:10:07 +0530

    Load products from the backend

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,11 +1,13 @@
 import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 // import '../data/cart-calss.js';
 import '../data/car.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
-
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+});
